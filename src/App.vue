15386db8433529b05bbe4e4567da069323d6e1a4
Zuruckt <zuruckyt@gmail.com>
2021-04-18 19:43:27 -0300

    wip

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <Navbar></Navbar>
+  <div class="content">
+    <img alt="Vue logo" src="./assets/logo.png">
+<!--    <Contador></Contador>-->
+    <CarTable></CarTable>
+  </div>
+
+</template>
+
+<script>
+require('bootstrap')
+import CarTable from "./components/CarTable";
+import Navbar from "./components/Navbar";
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    CarTable
+  }
+}
+
+
+</script>
+
+<style>
+@import "https://bootswatch.com/4/lux/bootstrap.min.css";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/Contador.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="form-group">
+    <button class="btn btn-primary btn-sm" @click="decrement">-</button>
+    <input type="text" v-model="count">
+    <button class="btn btn-primary btn-sm" @click="increment">+</button>
+    <button class="btn btn-primary btn-sm" @click="multiply">*</button>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    },
+    multiply() {
+    }
+    // fodase() {
+    //   fetch('http://127.0.0.1:8000/api/cars/').then(function (response) {
+    //     console.log(response.json().then(function (data) { return data}))
+    //   })
+    // }
+  },
+  name: "Contador"
+}
+</script>
+
+<style scoped>
+
+</style>
