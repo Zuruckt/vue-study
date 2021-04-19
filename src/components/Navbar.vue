8d823db7b8731c5395333c495b0dc74eb8f201b5
Zuruckt <zuruckyt@gmail.com>
2021-04-18 22:21:13 -0300

    wip

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,28 +8,10 @@
     <div class="collapse navbar-collapse" id="navbarColor01">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home
-            <span class="sr-only">(current)</span>
-          </a>
+            <router-link class="nav-link" to="/">Home</router-link>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Separated link</a>
-          </div>
+        <li class="nav-item active">
+          <router-link class="nav-link" to="/cars">Carros</router-link>
         </li>
       </ul>
       <form class="form-inline my-2 my-lg-0">
---
+++ src/views/Car.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <CarTable/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CarTable from "@/components/Car/CarTable";
+
+export default {
+  name: 'Home',
+  components: {
+    CarTable
+  }
+}
+</script>
